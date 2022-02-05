d5e2dabe1f99d2bfe3b59a42200b58f5333697f5
Ahmad-Fahad <ahmadsmfahad@gmail.com>
2022-02-05 19:37:20 +0600

    Excercise_Problem_Solving

--- sed.sh
+++ sed.sh
@@ -4,6 +4,7 @@ sed -e s/editor/tool/
 # replace all global
 sed -e s/editor/tool/g
 
+
 # highlight where match with {}
 # ex: atom is a good editor that I like most
 # ex: atom is a good {editor} that I like most
@@ -14,3 +15,8 @@ sed -e s/thy/your/ig
 
 # hilight {}
 sed -e s/thy/{\&}/ig
+
+
+# 8888 3678 9101 1232 ---> **** **** **** 1232
+# [0-9]{4} --> 4 digit  
+sed -r 's/[0-9]{4} /**** /g'
--- uniq.sh
+++ uniq.sh
@@ -1,3 +1,8 @@
 # frequency count
 uniq -c | cut -b7-
 
+#4  not like 11 11 22 22
+uniq -u
+
+#-i is for ignoring the case and c is for number of times the lines are repeated. Could u get why 7 is used??
+uniq -c -i | cut -c7-
